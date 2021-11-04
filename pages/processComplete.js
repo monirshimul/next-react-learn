0baa538e3f56a5d6ee9ebfd875184b56d19bd1df
monirshimul <monirshimul@gmail.com>
2021-11-04 15:47:47 +0600

    mobile view ready for processCom page

--- pages/processComplete.js
+++ pages/processComplete.js
@@ -17,11 +17,11 @@ import { Grid, IconButton, Paper } from '@mui/material';
 const ProcessComplete = () => {
     const [accID, setAccId] = useState();
     const [response, setResponse] = useState([
-        ["firstName", "monir"],
-        ["lastName", "shimul"],
-        ["email", "monirshimul@gmail.com"],
-        ["address", "mirpur"],
-        ["nationality", "Bangladesh"],
+        ["firstName", "monir", "#EBA83A"],
+        ["lastName", "shimul", "#00EAD3"],
+        ["email", "monirshimul@gmail.com", "#FF449F"],
+        ["address", "mirpur", "#F55C47"],
+        ["nationality", "Bangladesh", "#4AA96C"],
 
     ]);
 
@@ -31,22 +31,26 @@ const ProcessComplete = () => {
             <Grid container sx={{height: "100vh", display: "flex", justifyContent: "center",  alignItems: "center",}}>
                 <Grid item xs={10} sm={8} md={6}>
                     <Paper sx={{ padding: "15px" }}>
-                        <p style={{ fontSize: "25px", color: "#9D9D9D", textAlign: "center", borderBottom: "1px dashed #9d9d9d", padding: "10px 0px" }}>Process Complete Successfully</p>
-                        <p style={{ color: "#9D9D9D", textAlign: "center" }}>Here the Response Data</p>
+                        <Paper sx={{backgroundColor:"#E8F6EF"}}>
+                        <p style={{ fontSize: "20px", color: "#9D9D9D", textAlign: "center", padding: "10px 0px" }}>Process Complete Successfully</p>
+                        </Paper>
+                        
+                        <h5 style={{ color: "#9D9D9D", fontSize:"15px", textAlign: "center", marginTop:"10px" }}>Here the Response Data</h5><hr/>
 
                         <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
                             {
                                 response.map((val, ind) => (
                                     <Paper key={ind} sx={{ marginY: "5px" }}>
                                         <ListItem
+                                            sx={{backgroundColor:"#E8F6EF"}}
                                             secondaryAction={
                                                 <IconButton edge="end" aria-label="delete">
-                                                    <CheckIcon />
+                                                    <CheckIcon sx={{color:"#A3A847"}} />
                                                 </IconButton>
                                             }
                                         >
                                             <ListItemAvatar>
-                                                <Avatar>
+                                                <Avatar sx={{backgroundColor:val[2]}}>
                                                     <AlignHorizontalLeftIcon />
                                                 </Avatar>
                                             </ListItemAvatar>
